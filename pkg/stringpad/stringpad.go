ff0495875a4cbda9b861b302691e1858bbe7d4fa
Raphaël Piccolin <raphael.piccolin@dataiku.com>
2025-07-28 16:25:28 +0200

    feat: enhanced task display

---
+++ pkg/stringpad/stringpad.go
@@ -0,0 +1,15 @@
+package stringpad
+
+import "strings"
+
+// Adds padding up to the required space count.
+// If the string is longer than the space count, 1 space is added.
+func RightPad(str string, spaceCount uint8) string {
+	str = strings.TrimSpace(str)
+	padCount := int(spaceCount) - len(str)
+	if padCount < 1 {
+		return str + " "
+	} else {
+		return str + strings.Repeat(" ", padCount)
+	}
+}
