41eebbe2feb3858f87d3e51a7d2dc475180c6071
Raphael Piccolin <raphael.piccolin@outlook.com>
2026-02-13 22:19:41 +0100

    refactor: seperated out keybind handlers

--- internal/tui/controller/home.go
+++ internal/tui/controller/home.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"fmt"
-
 	"github.com/gdamore/tcell/v2"
 	"github.com/raphael-p/datashard/pkg/tviewcomponents/keybindmenu"
 	"github.com/rivo/tview"
@@ -10,65 +8,23 @@ import (
 
 func (c *Controller) Home() {
 	c.refreshTasks()
-	back := c.Home
-	quit := c.app.Stop
+	handler := keybindHandler{c}
 
 	home := tview.NewTextView().SetDynamicColors(true)
 	home.SetBorderPadding(1, 1, 2, 2)
 
 	keybindmenu.New().SetHighlighColour(tcell.ColorLimeGreen).
-		SetDefaultAction(func(commandName string) { c.infoPanel.Warn("invalid command: " + commandName) }).
-		AddKeybind('d', "dash", keybindmenu.DefaultBind, func(_ rune) {
-			c.startDash(c.app.Stop)
-		}).
-		AddKeybind('o', "open a task", keybindmenu.BindEnter, func(_ rune) {
-			if c.infoPanel.GetInput() != "" {
-				c.openTask(c.infoPanel.GetInput(), c.Home, c.app.Stop)
-			} else {
-				c.openTaskForm(c.Home, c.app.Stop)
-			}
-		}).
-		AddKeybind('b', "bump the priority of a task", keybindmenu.DefaultBind, func(_ rune) {
-			if c.infoPanel.GetInput() != "" {
-				c.bumpTask(c.infoPanel.GetInput())
-			} else {
-				c.bumpTaskForm(back, quit)
-			}
-		}).
-		AddKeybind('r', "remove a task", keybindmenu.DefaultBind, func(_ rune) {
-			if c.infoPanel.GetInput() != "" {
-				c.removeTask(c.infoPanel.GetInput())
-			} else {
-				c.removeTaskForm(back, quit)
-			}
-		}).
-		AddKeybind('a', "add a new task", keybindmenu.DefaultBind, func(_ rune) {
-			c.addTaskForm(back, quit)
-		}).
-		AddKeybind('j', "scroll down", keybindmenu.DefaultBind, func(_ rune) {
-			err := c.displayPanel.ScrollDown()
-			if err != nil {
-				c.infoPanel.Error(fmt.Errorf("failed to scroll down: %s", err))
-			}
-		}).
-		AddKeybind('k', "scroll up", keybindmenu.DefaultBind, func(_ rune) {
-			err := c.displayPanel.ScrollUp()
-			if err != nil {
-				c.infoPanel.Error(fmt.Errorf("failed to scroll up: %s", err))
-			}
-		}).
-		AddKeybind('q', "quit", keybindmenu.DefaultBind, func(_ rune) {
-			quit()
-		}).
-		AddKeybind(0, "", keybindmenu.BindNumber, func(r rune) {
-			c.infoPanel.SetInput(c.infoPanel.GetInput() + string(r))
-		}).
-		AddKeybind(0, "", keybindmenu.BindBackspace, func(r rune) {
-			currentInput := c.infoPanel.GetInput()
-			if currentInput != "" {
-				c.infoPanel.SetInput(currentInput[:len(currentInput)-1])
-			}
-		}).
+		SetFallback(func(commandName string) { c.infoPanel.Warn("invalid command: " + commandName) }).
+		AddKeybind('d', "dash", keybindmenu.DefaultBind, handler.dash).
+		AddKeybind('o', "open a task", keybindmenu.BindEnter, handler.open).
+		AddKeybind('b', "bump the priority of a task", keybindmenu.DefaultBind, handler.bump).
+		AddKeybind('r', "remove a task", keybindmenu.DefaultBind, handler.remove).
+		AddKeybind('a', "add a new task", keybindmenu.DefaultBind, handler.add).
+		AddKeybind('j', "scroll down", keybindmenu.DefaultBind, handler.scrollDown).
+		AddKeybind('k', "scroll up", keybindmenu.DefaultBind, handler.scrollUp).
+		AddKeybind('q', "quit", keybindmenu.DefaultBind, handler.quit).
+		AddKeybind(0, "or enter a task ID...", keybindmenu.BindNumber, handler.numberInput).
+		AddKeybind(0, "", keybindmenu.BindBackspace, handler.backspace).
 		SetFootnote("tip: you can type the task ID before invoking the open (o), bump (b), or remove (r) commands").
 		Apply(home)
 
---
+++ internal/tui/controller/keybindhandlers.go
@@ -0,0 +1,75 @@
+package controller
+
+import (
+	"fmt"
+)
+
+type keybindHandler struct {
+	c *Controller
+}
+
+func (ka *keybindHandler) add(_ rune) {
+	ka.c.addTaskForm(ka.c.Home, ka.c.app.Stop)
+}
+
+func (ka *keybindHandler) backspace(r rune) {
+	currentInput := ka.c.infoPanel.GetInput()
+	if currentInput != "" {
+		ka.c.infoPanel.SetInput(currentInput[:len(currentInput)-1])
+	}
+	if len(ka.c.infoPanel.GetInput()) == 0 {
+		ka.c.Home()
+	}
+}
+
+func (ka *keybindHandler) bump(_ rune) {
+	if ka.c.infoPanel.GetInput() != "" {
+		ka.c.bumpTask(ka.c.infoPanel.GetInput())
+	} else {
+		ka.c.bumpTaskForm(ka.c.Home, ka.c.app.Stop)
+	}
+}
+
+func (ka *keybindHandler) dash(_ rune) {
+	ka.c.startDash(ka.c.app.Stop)
+}
+
+func (ka *keybindHandler) numberInput(r rune) {
+	currentInput := ka.c.infoPanel.GetInput()
+	ka.c.infoPanel.SetInput(currentInput + string(r))
+	ka.c.Home()
+}
+
+func (ka *keybindHandler) open(_ rune) {
+	if ka.c.infoPanel.GetInput() != "" {
+		ka.c.openTask(ka.c.infoPanel.GetInput(), ka.c.Home, ka.c.app.Stop)
+	} else {
+		ka.c.openTaskForm(ka.c.Home, ka.c.app.Stop)
+	}
+}
+
+func (ka *keybindHandler) quit(_ rune) {
+	ka.c.app.Stop()
+}
+
+func (ka *keybindHandler) remove(_ rune) {
+	if ka.c.infoPanel.GetInput() != "" {
+		ka.c.removeTask(ka.c.infoPanel.GetInput())
+	} else {
+		ka.c.removeTaskForm(ka.c.Home, ka.c.app.Stop)
+	}
+}
+
+func (ka *keybindHandler) scrollDown(_ rune) {
+	err := ka.c.displayPanel.ScrollDown()
+	if err != nil {
+		ka.c.infoPanel.Error(fmt.Errorf("failed to scroll down: %s", err))
+	}
+}
+
+func (ka *keybindHandler) scrollUp(_ rune) {
+	err := ka.c.displayPanel.ScrollUp()
+	if err != nil {
+		ka.c.infoPanel.Error(fmt.Errorf("failed to scroll up: %s", err))
+	}
+}
