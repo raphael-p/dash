91143ff575004b6f1b4cc0c194d7eab29ccbc822
Raphael Piccolin <raphael.piccolin@outlook.com>
2026-08-09 21:35:06 +0200

    feat: confirmation before wiping db

--- TODO.md
+++ TODO.md
@@ -4,13 +4,11 @@ goal: make productivity tool which is not near a distraction (mobile, web)
 objective: build pomello timer + task manager in terminal
 
 - safeguard for task wiping
+- logs should be disabled by default
 - put on homebrew
 - history of completed tasks
     - give option to archive or delete
     - create view for archived tasks
 - potential logger bug: handle file count limit by deleting old files
-- rename project
-- README
-- logo
 - gameify task completion
 - terminal width-based display adjustments
--- cmd/dash/commands.go
+++ cmd/dash/commands.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/raphael-p/datashard/internal/database"
 )
+
+func getConfirmation(prompt string) bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s [y/yes]: ", prompt)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return false
+	}
+	input = strings.TrimSpace(strings.ToLower(input))
+	return input == "y" || input == "yes"
+}
 
 func handleCommand(command string) {
 	switch command {
 	case "init":
 		database.Initialise()
 	case "wipe":
-		database.Wipe()
+		if getConfirmation("are you sure you want to wipe the database?") {
+			database.Wipe()
+		} else {
+			fmt.Println("wipe cancelled")
+		}
 	case "generate":
 		sampleCmd := flag.NewFlagSet("generate", flag.ExitOnError)
 		randomEntryCount := sampleCmd.Int("randomEntryCount", 0, "number of random entries to generate on top of the default entries")
