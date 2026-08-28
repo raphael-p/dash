1f8e28b32e73fb8c49da8b6565abd5ad6a76aae5
Raphael Piccolin <raphael.piccolin@outlook.com>
2026-08-28 11:51:27 +0200

    fix: always get config file in data dir

--- cmd/dash/main.go
+++ cmd/dash/main.go
@@ -35,7 +35,7 @@ func main() {
 	logger.Create(dataDir, false)
 	defer logger.Close()
 
-	configreader.ReadConfigFile("datashard", dataDir, config)
+	configreader.ReadConfigFile(dataDir, config)
 
 	db, err := sql.Open("sqlite3", filepath.Join(dataDir, "datashard.db"))
 	if err != nil {
--- pkg/configreader/configreader.go
+++ pkg/configreader/configreader.go
@@ -7,22 +7,8 @@ import (
 	"path/filepath"
 )
 
-func getExecDirectory(executableName, fallback string) string {
-	if os.Args[0] == executableName {
-		// if executed from compiled binary, use its directory
-		ex, err := os.Executable()
-		if err != nil {
-			panic(fmt.Sprintf("failed to locate executable: %s", err))
-		}
-		return filepath.Dir(ex)
-	} else {
-		// otherwise, use fallback directory (relative path)
-		return fallback
-	}
-}
-
-func ReadConfigFile[T *Q, Q any](executableName, configRelativePath string, values T) {
-	configPath := filepath.Join(getExecDirectory(executableName, configRelativePath), "config.json")
+func ReadConfigFile[T *Q, Q any](dataDir string, values T) {
+	configPath := filepath.Join(dataDir, "config.json")
 	file, err := os.Open(configPath)
 	if err != nil {
 		panic(fmt.Sprintf("failed to open config file: %s", err))
